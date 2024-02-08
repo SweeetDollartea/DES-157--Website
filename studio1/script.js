f77f6430ec2b34f58b5689290a9a0700791fa89e
SweeetDollartea <asakurataira@gmail.com>
2024-02-07 23:06:29 -0800

    10th commit

--- studio1/script.js
+++ studio1/script.js
@@ -1,21 +1,23 @@
-document.addEventListener('DOMContentLoaded', function(){
-    var startbutton = document.querySelector(".button");
-
-startbutton.addEventListener ('click', function(){
-    document.getElementById("start-box").style.display='none';
-    document.getElementById('second-box').style.display = 'block';
-    document.getElementById('third-box').style.display = 'block';
-}) });
-
 (function(){
-    'use strict'
+    'use strict';
     console.log('reading js');
+
+    document.addEventListener('DOMContentLoaded', function(){
+        var startbutton = document.querySelector(".button");
+    
+    startbutton.addEventListener ('click', function(){
+        document.getElementById("start-box").style.display='none';
+        document.getElementById('second-box').style.display = 'block';
+    });
+
+    var form =document.querySelector('form');
+    
+    form.addEventListener('submit', function(event){
+        event.preventDefault();
 
     const form =document.querySelector('form');
     const lib =document.querySelector("second-box");
-
-    form.addEventListener('submit', function(event){
-        event.preventDefault();
+    const libfinal =document.querySelector('#finalbox');
 
     const cn = document.getElementById("charactername").value;
     const vn = document.getElementById('villainname').value;
@@ -24,10 +26,16 @@ startbutton.addEventListener ('click', function(){
     const noun = document.getElementById('noun').value;
     const pluralnoun = document.getElementById('pluralnoun').value;
 
-    let mytext;
+    const ad = document.getElementById('adjective1').value;
+    const v = document.getElementById('verb').value;
+    const ad2 =document.getElementById('adjective2').value;
+    const v2 =document.getElementById('verb2').value;
+    const ad3 =document.getElementById('adjective3').value;
+
+    let mytext= `The words submitted are ${cn}`;
 
     if (cn ==""){
-        mytext="please provide a charactername";
+        mytext="please provide a character name";
         document.getElementById(charactername).focus();
     }
     else if (vn ==""){
@@ -50,15 +58,39 @@ startbutton.addEventListener ('click', function(){
         mytext="Please provide a plural noun";
         document.getElementById(pluralnoun).focus();
     }
-    else {
-        mytext= (`The words submitted are ${charactername}, ${villainname}, ${charactername2}, ${hisorher}, ${noun}, ${pluralnoun}`);
-        const textfield =document.querySelectorAll('input[type=text]');
-        for (let i=0; i<textfield.length;i++){
-            textfield[i].value='';
-        }
+    else if (ad==""){
+        mytext="please provide an adjective"
+        document.getElementById(adjective1).focus();
     }
-    form.innerHTML=mytext;
+    else if (v==""){
+        mytext="please provide a verb"
+        document.getElementById(verb).focus();
+    }
+    else if (ad2==""){
+        mytext="please provide another adjective"
+        document.getElementById(adjective2).focus();
+    }
+    else if (v2==""){
+        mytext="please provide another verb"
+        document.getElementById(verb2).focus();
+    }
+    else if (ad3==""){
+        mytext="please provide another adjective"
+        document.getElementById(adjective3).focus();
+    }
+    else {
+        mytext = (`In the Mushroom Kingdom,${cn}, a ${ad} plumber, discoverd that Princess ${cn2} was kidnapped by the evil ${vn}.
+        To save her, ${cn} must travel through the ${ad2} forest, filled with ${pluralnoun}. Along the way, ${cn} finds a magical ${noun} that gives power to ${v}.
+        After overcoming obstacles by defeating Koopa Troops and Bowser Jr., ${cn}, confronts ${vn} in a ${ad3} castle.
+        Using ${hoh} new power to ${v}, ${cn} defeats ${vn} and rescue Princess ${cn2} becoming a hero of Mushroom Kingdom.`);
+       
+    const libfinal = document.getElementById('finalbox');
+    libfinal.innerHTML=`<p class="final">${mytext}</p>`;
+    libfinal.style.display= 'block';
 
-    });
-
+    document.getElementById('second-box').style.display = 'none';
+    }
+});
+});
 })();
+
